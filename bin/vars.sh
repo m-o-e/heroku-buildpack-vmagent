d1624e6184de9aaac7fae40e20dacfe5a086f8b8
Joey <joey.wang@ably.com>
2022-07-20 10:42:27 +0100

    set default version to 1.79.0

--- bin/vars.sh
+++ bin/vars.sh
@@ -1,8 +1,8 @@
 # The version of vmagent to install, check https://github.com/VictoriaMetrics/VictoriaMetrics/releases
-VMAGENT_VERSION="${VMAGENT_VERSION:-v1.75.0}"
+VMAGENT_VERSION="${VMAGENT_VERSION:-v1.79.0}"
 
 # The SHA256 hash of the "vmagent-prod" binary extracted from the vmutils archive.
 #
 # The value can be found in the released vmutils checksum file, for example in:
-# https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v1.63.0/vmutils-amd64-v1.63.0_checksums.txt
-VMAGENT_SHA256="${VMAGENT_SHA256:-2ce1aabaa96f46f5f4412a49d9a4cf4fb0e0b05873a7a9a7255b93bc746486eb}"
+# https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v1.79.0/vmutils-linux-arm64-v1.79.0_checksums.txt
+VMAGENT_SHA256="${VMAGENT_SHA256:-bedd743eb4ceb8be920fa8124cf7e060981e614203a84cf1b5a31a717aa4b8df}"
